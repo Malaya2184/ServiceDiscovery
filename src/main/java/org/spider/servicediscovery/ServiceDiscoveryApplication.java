75936d8541cfb586194e74b301c8b964e80a5091
Malaya2184 <malayakumarswain8@gmail.com>
2024-08-08 02:38:02 +0530

    created eureka server for the other eureka client regestration

--- src/main/java/org/spider/servicediscovery/ServiceDiscoveryApplication.java
+++ src/main/java/org/spider/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package org.spider.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
 
 }
